05489112c65d3beb694d63c1ad54de3edf270d50
haile <haileleulfiseha@gmail.com>
2024-09-24 15:37:02 +0300

    now the game loads to the home-page

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,11 @@ import { Renderer } from "./renderer";
 import { HomePage } from "./homepage";
 
 async function main() {
-    const renderer = new Renderer();
-    await renderer.init();
-    // const grid = new Grid(renderer);
-    // await grid.init();
-    const homePage = new HomePage(renderer);
-    await homePage.init();
+	const renderer = new Renderer();
+	await renderer.init();
+	// const grid = new Grid(renderer);
+	// await grid.init();
+	const homePage = new HomePage(renderer);
+	await homePage.init();
 }
 main();
